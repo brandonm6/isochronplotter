fed74227ba4c87b8a1b3fee45f244ef4a5000c07
Brandon Maher <brandmaher@gmail.com>
2021-08-04 20:45:16 -0400

    added a status option for omitted steps, changed self.splits from dicitonary to list of run id #s

--- builddataframe.py
+++ builddataframe.py
@@ -5,16 +5,23 @@ import numpy as np
 class BuildDataframe:
     # unpacks and builds dataframe
 
-    def __init__(self, csv_path, remove, verbose=True):
-        self.verbose = verbose
-        self.removed = remove
+    def __init__(self, csv_path, omit):
+        self.omit = omit
+
+        # run ids removed from the data set (NaN values)
+        self.force_removed = None
         self.j_val = None
+        # dataframe of steps that correspond with their run ids
         self.step_key = None
+
         self.orig_df = pd.read_csv(csv_path)
+
         # main df with corrected values
         self.main_df = self.build_main()
-        # dictionary of Run ID#s and dataframes for each #
+        # list of Run ID#s
         self.splits = self.find_splits()
+
+        self.update_omit()
 
     def build_main(self):
         # remove empty rows and correct column names
@@ -26,17 +33,15 @@ class BuildDataframe:
 
         self.j_val = float(self.orig_df["J"].iloc[0])
         self.orig_df["Step"] = np.arange(len(self.orig_df)) + 1
-        self.orig_df["Ltr"] = self.orig_df["Run_ID"].str[-1]
+        self.orig_df["Status"] = "OK"
 
         # grab select columns
         grabbed = self.orig_df[
-            ["Step", "Run_ID", "Ltr", "Isoch_39_Over_40", "Pct_i39_Over_40_Er", "Isoch_36_Over_40",
-             "Pct_i36_Over_40_Er",
-             "Correl_36_Over_39", "Ar36_Over_Ar39", "Ar39_Moles"]]
+            ["Step", "Run_ID", "Status", "Isoch_39_Over_40", "Pct_i39_Over_40_Er", "Isoch_36_Over_40",
+             "Pct_i36_Over_40_Er", "Correl_36_Over_39", "Ar36_Over_Ar39", "Ar39_Moles", "Age_Er"]]
 
-        # drop self.removed values and NaN values
-        grabbed = grabbed[~grabbed["Ltr"].isin(self.removed)].reset_index(drop=True)
-        self.removed += list(grabbed["Run_ID"][set(np.where(grabbed.isna())[0])])
+        # drop NaN values
+        self.force_removed = list(grabbed["Run_ID"][set(np.where(grabbed.isna())[0])])
         grabbed = grabbed.dropna().reset_index(drop=True)
 
         # convert string values to floats
@@ -46,40 +51,33 @@ class BuildDataframe:
         grabbed["Pct_i36_Over_40_Er"] = (grabbed["Pct_i36_Over_40_Er"] / 100) * grabbed["Isoch_36_Over_40"]
 
         # rename columns for convenience
-        grabbed.columns = ["Step", "Run ID", "Ltr", "39Ar/40Ar", "39Ar/40Ar er", "36Ar/40Ar", "36Ar/40Ar er",
-                           "corr 36/39", "36Ar/39Ar", "Ar39 moles"]
+        grabbed.columns = ["Step", "Run ID", "Status", "39Ar/40Ar", "39Ar/40Ar er", "36Ar/40Ar", "36Ar/40Ar er",
+                           "corr 36/39", "36Ar/39Ar", "Ar39 moles", "Age er"]
 
         self.step_key = grabbed[["Step", "Run ID"]]
 
         return grabbed
 
-    def update_main(self, splits):
-        """
-        updates main dataframe by adding 39Ar cumulative percentage column (%39Ark)
-        if there are splits in the sample
-        """
-        tmp = pd.DataFrame()
-        for split in np.sort(list(splits.keys())):
-            tmp = pd.concat([tmp, splits.get(split)["%39Ark"]])
-        self.main_df["%39Ark"] = tmp.reset_index(drop=True)
-
     def find_splits(self):
-        # check if there are splits in the sample
-        id_nums = set([run_id[:run_id.find(next(filter(str.isalpha, run_id)))] for run_id in self.main_df["Run ID"]])
-        if len(id_nums) == len(self.main_df["Run ID"]):
-            if self.verbose:
-                print("The sample contains no splits.")
-            return
+        # find splits in the sample
+        id_nums = np.sort(
+            list(set([run_id[:run_id.find(next(filter(str.isalpha, run_id)))] for run_id in self.main_df["Run ID"]])))
 
-        splits = {}
+        pct_39ark = np.array([])
         for num in id_nums:
-            # builds splits dataframes
+            # builds splits dataframes to get cum %39Ar
             steps = np.where(self.main_df["Run ID"].str.contains(num))[0]
-            df = self.main_df.iloc[int(steps[0]):int(steps[-1]) + 1]
+            tmp = self.main_df.iloc[int(steps[0]):int(steps[-1]) + 1]
+            pct_39ark = np.append(pct_39ark, ((tmp["Ar39 moles"] / tmp["Ar39 moles"].sum()) * 100).cumsum().to_numpy())
 
-            df["%39Ark"] = ((df["Ar39 moles"] / df["Ar39 moles"].sum()) * 100).cumsum()
-            splits[num] = df.drop("Ar39 moles", axis=1).reset_index(drop=True)
+        # updates main dataframe with cum %39Ark column
+        self.main_df["%39Ark"] = pct_39ark
 
-        self.update_main(splits)
+        return list(id_nums)
 
-        return splits
+    def update_omit(self):
+        # updates status of omitted letters in main_df
+        for num in self.main_df["Run ID"]:
+            # check if run id letter is in omit
+            if num[-1] in self.omit:
+                self.main_df["Status"].iloc[self.main_df["Run ID"][self.main_df["Run ID"] == num].index] = "Omitted"
