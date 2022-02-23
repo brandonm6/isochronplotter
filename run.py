55779b6724bc375631d3ab5ff10c8cd58930d3eb
Brandon Maher <brandmaher@gmail.com>
2022-02-22 23:15:46 -0500

    simplified

--- run.py
+++ run.py
@@ -1,18 +1,9 @@
 import os
-import sys
-import time
-
-import matplotlib.pyplot as plt
-
 from plotter.plotter import Plotter
 
+file_name = "CIA dense 197'.csv"
 dir_path = os.path.dirname(os.path.realpath("isochronplotter"))
-csv_path = os.path.join(dir_path, "csvs", "CIA dense 197'.csv")
+csv_path = os.path.join(dir_path, "csvs", file_name)
 
-t0 = time.time()
 iso = Plotter(csv_path, omit=None, verbose=False, iverbose=False)
 iso.plot()
-tf = time.time()
-print("\nrun time:", tf-t0)
-
-plt.show()
