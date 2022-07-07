90774f879a872b2d1578963f592f14617d5ae2d6
Brandon Maher <brandmaher@gmail.com>
2022-07-07 16:55:49 -0400

    changed type

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ file_name = "CIA dense 197'.csv"
 dir_path = os.path.dirname(os.path.realpath("isochronplotter"))
 csv_path = os.path.join(dir_path, "csvs", file_name)
 
-iso = Plotter(csv_path, omit=None, verbose=False, iverbose=False)
+iso = Plotter(csv_path, omit=[], verbose=False, iverbose=False)
 iso.plot()
